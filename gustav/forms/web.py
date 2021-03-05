4ce8364e2054b6a71bd323ce9541218ffaf6547f
kbsezginel <kbsezginel@othot.com>
2021-03-05 00:38:57 -0500

    cleanup, feedback duration and trial output vars

--- gustav/forms/web.py
+++ gustav/forms/web.py
@@ -26,6 +26,7 @@ class Interface():
         self.upper_left_text = ''
         self.lower_left_text = ''
         self.lower_right_text = ''
+        self.feedback_duration = 1000
 
         self.checked = []
 
@@ -122,8 +123,8 @@ class Interface():
         return {'id': self.id}
 
     def present_trial(self, exp):
-        self.lower_left_text = 'Trial: {}'.format(exp.run.trials_block)
-        self.lower_right_text = f"Block {exp.run.block+1} of {exp.var.nblocks+1}"
+        # self.lower_left_text = 'Trial: {}'.format(exp.run.trials_block)
+        # self.lower_right_text = f"Block {exp.run.block + 1} of {exp.var.nblocks}"
         output = {
                     'type': 'trial',
                     'lower_left_text': self.lower_left_text,
@@ -134,7 +135,7 @@ class Interface():
                     'prompt2': self.prompt2,
                     'answer': exp.var.dynamic['correct'],
                     'delay': exp.user.isi,
-                    'next_delay': 2000,
+                    'next_delay': self.feedback_duration,
                     'block': exp.run.block,
                     'trial': exp.run.trials_block
                   }
@@ -234,153 +235,3 @@ class Interface():
     def destroy(self):
         # Stop experiment
         pass
-
-    def update(self, filename="test.json"):
-        """
-        Update the interface
-        """
-        self.out = {}
-        with open(filename, 'w') as f:
-            json.dump(self.out, f)
-
-    # Use instead of update_Status_Right etc
-    def update_text(self):
-        pass
-
-    def show_Notify_Left(self, show=None):
-        """Show the left notify text
-
-            If show==None, toggle.
-            Otherwise show should be a bool.
-        """
-        if show is not None:
-            self.notify_l_show = show
-        else:
-            self.notify_l_show = not self.notify_l_show
-
-    def update_Notify_Right(self, s, show=None):
-        """Update the notify text to the left of the face.
-
-            show is a bool specifying whether to show the text,
-            set to None to leave this param unchanged [default].
-            show can also be set with show_Notify_Left.
-        """
-        self.notify_r_str = s
-        if show is not None:
-            self.notify_r_show = show
-
-    def show_Notify_Right(self, show=None):
-        """Show the right notify text
-
-            If show==None, toggle.
-            Otherwise show should be a bool.
-        """
-
-        if show is not None:
-            self.notify_r_show = show
-        else:
-            self.notify_r_show = not self.notify_r_show
-
-    def update_Notify_Right(self, s, show=None):
-        """Update the notify text to the left of the face.
-
-            show is a bool specifying whether to show the text,
-            set to None to leave this param unchanged [default].
-            show can also be set with show_Notify_Left.
-        """
-        self.notify_r_str = s
-        if show is not None:
-            self.notify_r_show = show
-
-    def show_Buttons(self, show=None):
-        """Show the position bar
-
-           If show==None, toggle show.
-           Otherwise show should be a bool.
-        """
-        if show is not None:
-            self.buttons_show = show
-        else:
-            self.buttons_show = not self.buttons_show
-
-    def show_Prompt(self, show=None):
-        """Show the prompt
-
-           If show==None, toggle show.
-           Otherwise show should be a bool.
-        """
-        if show is not None:
-            self.prompt_show = show
-        else:
-            self.prompt_show = not self.prompt_show
-
-    def update_Prompt(self, s, show=True):
-        """Update the text of the prompt
-
-            redraw is a bool specifying whether to redraw window.
-            A window redraw can also be set with update.
-        """
-        self.prompt = s
-        self.prompt_show = show
-
-    def show_Buttons(self, show=None):
-        """Show the position bar
-
-           If show==None, toggle show and force a redraw. Otherwise
-            show should be a bool.
-
-            redraw is a bool specifying whether to redraw window.
-            A window redraw can also be set with update.
-        """
-        if show is not None:
-            self.buttons_show = show
-        else:
-            self.buttons_show = not self.buttons_show
-
-    def update_Status_Left(self, s):
-        """Update the text on the left side of the status bar
-
-            redraw is a bool specifying whether to redraw window.
-            A window redraw can also be set with update.
-        """
-        self.status_l_str = s
-
-    def update_Status_Right(self, s):
-        """Update the text on the right side of the status bar
-
-            redraw is a bool specifying whether to redraw window.
-            A window redraw can also be set with update.
-        """
-        self.status_r_str = s
-
-    def update_Status_Center(self, s):
-        """Update the text in the center of the status bar
-
-            redraw is a bool specifying whether to redraw window.
-            A window redraw can also be set with update.
-        """
-        self.status_c_str = s
-
-    def update_Title_Left(self, s):
-        """Update the text on the left side of the title bar
-
-            redraw is a bool specifying whether to redraw window.
-            A window redraw can also be set with update.
-        """
-        self.title_l_str = s
-
-    def update_Title_Right(self, s):
-        """Update the text on the right side of the title bar
-
-            redraw is a bool specifying whether to redraw window.
-            A window redraw can also be set with update.
-        """
-        self.title_r_str = s
-
-    def update_Title_Center(self, s):
-        """Update the text in the center of the title bar
-
-            redraw is a bool specifying whether to redraw window.
-            A window redraw can also be set with update.
-        """
-        self.title_c_str = s
